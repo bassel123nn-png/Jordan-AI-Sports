96232363229f6b7dc56d9f542a6534ff2f5bdba6
bassel123nn-png <bassel123.nn@gmail.com>
2026-07-31 17:06:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.subheader("التحليل المدعوم بالذكاء الاصطناعي ل
 
 # مفاتيح الربط مع سترافا
 CLIENT_ID = "268965"
-CLIENT_SECRET = "6a94357c4df6f6d743a72a885a13d132ca911ec1
+CLIENT_SECRET = "6a94357c4df6f6d743a72a885a13d132ca911ec1"
 
 "
 REDIRECT_URI = "https://jordan-ai-sports-4n5tndhgkesysquygzwyhg.streamlit.app/"
